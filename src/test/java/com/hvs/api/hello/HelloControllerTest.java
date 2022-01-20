ba53a017e7638cb067baaef8c48a71fd91adc34a
Henry Martinez <hmartinez@novopayment.com>
2022-01-20 16:20:34 -0500

    fix giotignore

--- src/test/java/com/hvs/api/hello/HelloControllerTest.java
+++ src/test/java/com/hvs/api/hello/HelloControllerTest.java
@@ -1,10 +1,19 @@
 package com.hvs.api.hello;
 
+import org.junit.jupiter.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(classes = HelloController.class)
 class HelloControllerTest {
+
+    @Autowired
+    private HelloController controller;
 
     @org.junit.jupiter.api.Test
     void sayHello() {
+        Assertions.assertEquals("Hello there!!!", controller.sayHello());
     }
 }
